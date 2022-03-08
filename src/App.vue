22e6aa6c1edce842e8c90b477c068852850f5cae
Benjamin Winters <bwinte19@byu.edu>
2022-03-07 20:17:25 -0700

    Add header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,109 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+<div id="app">
+  <div class="bdy">
+    <div class="page">
+      <div id="menu">
+        <div id="brand">
+          <router-link to="/">
+            <img src="./assets/film.png">
+          </router-link>
+        </div>
+        <div class="company-name">
+        <h1>See A Film</h1>
+        </div>
+          <router-link to="/">
+            <div class="menu-item">
+              <p>Movies</p>
+            </div>
+          </router-link>
+          <router-link to="/theatres">
+            <div class="menu-item">
+              <p>Theatres</p>
+            </div>
+          </router-link>
+      </div>
+      <router-view />
+    </div>
+    <footer><p>Code for this project can be found <a href="https://github.com/bwinte19/theatre-site" target="_blank">here</a></p></footer>
   </div>
+</div>
+
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+body {
+  margin: 0px;
+}
+
+#menu {
+  display: flex;
+  background-color: #FFE900;
+  padding: 20px;
+  margin-bottom: 50px;
+}
+
+#brand img {
+  height: 50px;
+  margin-right: 20px;
+}
+
+.company-name {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-right: auto;
+}
+
+.menu-item {
+  display: flex;
+  padding: 15px 20px;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 10px;
+}
+
+.menu-item:hover {
+  background-color: rgb(155, 154, 154);
+  color: white;
+}
+
+.menu-item p {
+  margin: 0px;
+}
+
+a {
+  text-decoration: none;
+  color: blue;
+}
+
+html, body {
+  height: 100%;
+}
+
+.bdy {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+}
+.page {
+  flex: 1 0 auto;
+}
+
+footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  font-size: 20px;
+  margin-top: 50px;
+  height: 100px;
+  flex-shrink: 0;
+  background-color: #FFE900;
 }
 </style>
---
+++ src/mock.js
@@ -0,0 +1,112 @@
+let imgWidth = 250;
+let imgHeight = 300;
+let baconIpsum = "Bacon ipsum dolor amet tenderloin buffalo leberkas capicola, biltong short loin prosciutto bacon beef pork loin shankle venison. Tri-tip boudin pork chop shank spare ribs prosciutto, porchetta frankfurter bresaola rump fatback. Salami ground round turducken ball tip t-bone alcatra ham venison short loin chicken boudin drumstick picanha tenderloin pork. Shank jowl buffalo turkey, ribeye brisket shoulder chislic pig porchetta ball tip pork chop bresaola frankfurter ham hock.";
+
+export const mockMovies = [
+  {
+    id: 0,
+    name: "Bambi Reimagined",
+    description: "Bambi Reimagined is the movie no one deserved but everyone needed." + baconIpsum,
+    url: "https://picsum.photos/id/1003/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 1,
+    name: "Speculate on Life",
+    description: "Join this man on his journey of staring at the ocean for 4 hours." + baconIpsum,
+    url: "https://picsum.photos/id/1005/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 2,
+    name: "Your Generic Chick Flick",
+    description: "You know how it goes.  Guy likes girl, girl likes guy, guy does something to make girl mad, they make up, they get married." + baconIpsum,
+    url: "https://picsum.photos/id/1004/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 3,
+    name: "Nature I",
+    description: "See all the magic of nature in the first installment.  More will come if people can possibly fund me." + baconIpsum,
+    url: "https://picsum.photos/id/1015/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 4,
+    name: "Hobo Dog",
+    description: "When man fails to be the dog's best friend, he lives in the forest...in a blanket." + baconIpsum,
+    url: "https://picsum.photos/id/1025/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 5,
+    name: "Climbing to the Top",
+    description: "Some dude climbed a mountain...felt the need to film it." + baconIpsum,
+    url: "https://picsum.photos/id/1036/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 6,
+    name: "Death by City",
+    description: "Can't stand the city...wanna live in the country the rest of my life...cities are so depressing." + baconIpsum,
+    url: "https://picsum.photos/id/1048/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 7,
+    name: "Wanna be Lion King",
+    description: "Disney's first cut of the live action Lion King.  Didn't end up very good." + baconIpsum,
+    url: "https://picsum.photos/id/1074/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 8,
+    name: "Speed Biker",
+    description: "Pedal...Pedal...Pedal...........Super Fast............." + baconIpsum,
+    url: "https://picsum.photos/id/1077/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 9,
+    name: "Light at the end of the tunnel",
+    description: "Don't go towards the light.  We have cookies!" + baconIpsum,
+    url: "https://picsum.photos/id/137/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 10,
+    name: "Something about Music",
+    description: "The cover photo shows a guitar, so you'll probably hear some music in this film." + baconIpsum,
+    url: "https://picsum.photos/id/145/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 11,
+    name: "Iphone Infomercial",
+    description: "Big companies like Apple aren't invasive enough with their TV commercials, so we thought we'd get you to pay money to watch their ad." + baconIpsum,
+    url: "https://picsum.photos/id/160/" + imgWidth + "/" + imgHeight,
+  },
+  {
+    id: 12,
+    name: "Rocks",
+    description: "Rocks.  That's it...that's all you're gonna see in here." + baconIpsum,
+    url: "https://picsum.photos/id/168/" + imgWidth + "/" + imgHeight,
+  }
+];
+
+export const mockLocations = [
+  {
+    id: 0,
+    name: "SeeAFilm 1",
+    address: "111 street str.  Provo, UT 84604",
+  },
+  {
+    id: 1,
+    name: "SeeAFilm 33",
+    address: "823 notAStreet str.  Provo, UT 84604",
+  },
+  {
+    id: 2,
+    name: "SeeAFilm 11",
+    address: "14 whyinacircle cir.  Provo, UT 84604",
+  },
+  {
+    id: 3,
+    name: "SeeAFilm 20334",
+    address: "11122 reallyfarsouth str.  Provo, UT 84604",
+  },
+  {
+    id: 4,
+    name: "SeeAFilm -223",
+    address: "-1234 overflow str.  Provo, UT 84604",
+  },
+];
